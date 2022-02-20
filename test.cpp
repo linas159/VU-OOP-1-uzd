09dd9587a58b720d82d244cd571199829fbd07d1
Linas <linas.mintautiskis@gmail.com>
2022-02-20 15:14:52 +0200

    trecia dalis nezinomas n (TAIP PAT PATAISYTA 2 SKAICIAI PO KABLELIO)

--- test.cpp
+++ test.cpp
@@ -6,7 +6,6 @@ struct studentas
 {
     string vard;
     string pavard;
-    int n;
     vector <int> nd;
     int egz;
     float gal;
@@ -43,43 +42,36 @@ void ivestis(int &k, vector <studentas> &stud, bool &ar)
                 ar=false;
                 break;
             }
-            cout<<"Kiek studentas turi pazymiu: ";
-            cin>>stud[i].n;
-            if (stud[i].n==0)
+            cout<<"Pazymiai (Noredami baigti ivedineti iveskite '0'): ";
+            int a=1,kiek=0;
+            float vid=0,med;
+            while (a!=0)
+            {
+                cin>>a;
+                if (a==0)
+                {
+                    break;
+                }
+                stud[i].nd.push_back(a);
+                vid=vid+a;
+                kiek++;
+            }
+            if (kiek==0)
             {
                 cout<<"Ivesta klaidingai"<<endl;
                 ar=false;
                 break;
             }
-            cout<<"Pazymiai: ";
-            int a;
-            float vid=0,med;
-            for (int j=0;j<stud[i].n;j++)
-            {
-                cin>>a;
-                if (a==0 || a>10)
-                {
-                    cout<<"Ivesta klaidingai"<<endl;
-                    ar=false;
-                    break;
-                }
-                stud[i].nd.push_back(a);
-                vid=vid+a;
-            }
-            if (a==0)
-            {
-                break;
-            }
             sort(stud[i].nd.begin(), stud[i].nd.end());
-            if (stud[i].n%2==0)
+            if (kiek%2==0)
             {
-                med=(stud[i].nd[stud[i].n/2]+stud[i].nd[stud[i].n/2-1])/2.0;
+                med=(stud[i].nd[kiek/2]+stud[i].nd[kiek/2-1])/2.0;
             }
             else
             {
-                med=stud[i].nd[stud[i].n/2];
+                med=stud[i].nd[kiek/2];
             }
-            vid=vid/stud[i].n;
+            vid=vid/kiek;
             cout<<"Egzamino rezultatas: ";
             cin>>stud[i].egz;
             if (stud[i].egz==0)
@@ -101,7 +93,7 @@ void isvestis(int k, vector <studentas> &stud)
     cout << "|"<< left << setw(20) << "Vardas" << "|" << left << setw(20) << "Pavarde" << "|" << left << setw(20) << "Galutinis (Vid.)"<< "|" << left << setw(20) << "Galutinis (Med.)" << endl;
     for (int i=0;i<k;i++)
     {
-        cout << "|"<< left << setw(20) << stud[i].vard << "|" << left << setw(20) << stud[i].pavard << "|" << left << setw(20) << stud[i].gal<< "|" << left << setw(20) << stud[i].galmed<< endl;
+        cout << "|"<< left << setw(20) << stud[i].vard << "|" << left << setw(20) << stud[i].pavard << "|" << left << setw(20) << fixed << setprecision(2) << stud[i].gal<< "|" << left << setw(20) << fixed << setprecision(2) << stud[i].galmed<< endl;
     }
 }
 
