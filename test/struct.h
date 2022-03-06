3a03ad3d3d043c84496f4c39cb71758e4937939d
Linas <linas.mintautiskis@gmail.com>
2022-03-06 13:14:10 +0200

    header failai

---
+++ test/struct.h
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ifstream in("kursiokai.txt");
+
+struct studentas
+{
+    string vard;
+    string pavard;
+    vector <int> nd;
+    int egz;
+    float gal;
+    float galmed;
+};
